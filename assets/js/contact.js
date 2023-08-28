e5ba8db56f8d0a4322ea03b51a25a39b0b60a274
Ilyes1 <ihannouch7@gmail.com>
2023-08-28 09:27:54 +0100

    changes

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -249,56 +249,14 @@ Price: £${price}`
 		emailjs.send('service_5ibjqp3', 'template_bhj2bbm', templateParams)
 		.then(function(response) {
 				console.log('SUCCESS!', response.status, response.text);
-				alert('Message sent successfully')
+				// alert('Message sent successfully')
+				window.location.pathname = '/contact-success.html'
 		}, function(error) {
 			console.log('FAILED...', error);
 		});
 	})
 
 
-	$('.application-form').submit(function(e) {
-        e.preventDefault()
-
-        alert($('#ukLicense').is(':checked'))
-
-        // const data = {
-        //     name: $('#name').val(),
-        //     surname: $('#surname').val(),
-        //     address: $('#address').val(),
-        //     postcode: $('#postcode').val(),
-        //     license: $('#ukLicense').is(':checked') ? 'UK' : 'EU',
-        //     vehicle: $('#vehicle span').text(),
-        //     phone: $('#phone').val(),
-        //     email: $('#email').val()
-        // };
-
-        // emailjs.send('service_mcguz7k', 'template_ci7zx4l', data)
-		// .then(function(response) {
-        //     window.location.pathname = '/application-success.html'
-		// }, function(error) {
-		// 	console.log('FAILED...', error);
-		// });
-          
-        // Add one line to the sheet
-        // fetch("https://sheet.best/api/sheets/359d117b-bf00-4e7b-bfad-346d05010bdb", {
-        //     method: "POST",
-        //     mode: "cors",
-        //     headers: {
-        //         "Content-Type": "application/json",
-        //     },
-        //     body: JSON.stringify(data),
-        // })
-        // .then((r) => r.json())
-        // .then((data) => {
-        //     // The response comes here
-        //     console.log(data);
-        //     window.location.pathname = '/application-success.html'
-        // })
-        // .catch((error) => {
-        //     // Errors are reported there
-        //     console.log(error);
-        // });
-
-    })
+	
 
 });
